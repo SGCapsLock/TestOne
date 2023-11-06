f07d598a7c36e7917d43b030b310d5db78bb5013
TesterOne <Gorovoyac@gmail.com>
2023-11-06 12:33:36 +0300

    TestOne done 1.0

---
+++ searchtest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @BeforeAll
+    public static void setUp() {
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
